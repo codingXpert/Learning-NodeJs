ee0309bd397ee085835db21db141f65dc3e178c3
Vivek <vivekraj78965.vr@gmail.com>
2022-06-18 18:10:28 +0530

    importing variables from other file

--- addition.js
+++ addition.js
@@ -1,3 +1,8 @@
 let a = 20;
 let b  = 30;
 console.log("Sum = " , a+b);
+
+module.exports = {
+    c : 50,
+    d : 'XYZ'
+}
---
+++ import var from other file.js
@@ -0,0 +1,5 @@
+const impo= require('./addition');
+console.log(impo);
+console.log(impo.c);
+console.log(impo.d);
+
