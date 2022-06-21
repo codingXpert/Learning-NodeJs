78188d3edc78a38c0f8a917a30cb07dd432ec6a7
Vivek <vivekraj78965.vr@gmail.com>
2022-06-21 19:22:50 +0530

    adding comment

--- command line.js
+++ command line.js
@@ -1,3 +1,11 @@
 const fs = require('fs');
 var input = process.argv;
 fs.writeFileSync(input[2] , input[3]); 
+
+
+
+
+// in terminal write file_name from where we are creating new file as follows
+// node '.\command line.js' apple.txt 'this is fruit'
+
+// And this will create a new file with name apple.txt and data inside it "this is fruit"
