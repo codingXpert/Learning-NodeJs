06d67a9e71c9b2bfe9833072581cfe6965a618a6
Vivek <vivekraj78965.vr@gmail.com>
2022-06-26 23:09:14 +0530

    Promise Ex2

---
+++ promise2.js
@@ -0,0 +1,21 @@
+let myPromise = new Promise((resolve , reject) => {
+    const x = 'abc';
+    const y = 'abc';
+
+    if(x === y){
+        resolve();
+    }else{
+        reject();
+    }
+
+});
+
+myPromise
+       .then(() =>{
+        console.log('Success');
+       });
+
+myPromise
+       .catch(() => {
+        console.log('Some error has occured');
+       });
