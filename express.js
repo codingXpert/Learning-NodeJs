e772bc138c38de199ebcadb8437060d9d6635b51
Vivek <vivekraj78965.vr@gmail.com>
2022-06-27 14:42:05 +0530

    installing and express

---
+++ express.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+
+app.get('' , (req , res) => {
+    res.send('<h1>Welcome , this is home page</h1>');
+});
+
+app.get('/about' ,  (req , res) => {
+    res.end('<h1>Welcome , this is about page</h1>');
+});
+
+app.get('/help' , (req , res) => {
+    res.send('<h1>Welcome this is help page</h1>');
+})
+
+app.listen(8000 , () => {
+    console.log('Server running on the port 8000')
+});
