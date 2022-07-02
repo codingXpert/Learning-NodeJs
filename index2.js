2af19d4b7930df8789bbad33a78a8ba0ba82f1a2
Vivek <vivekraj78965.vr@gmail.com>
2022-07-02 19:50:33 +0530

    applying for Each loop on array list

---
+++ index2.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+
+const port = 8000;
+const app = express();
+
+//Telling Node Js that ue are using view engine of ejs
+app.set('view engine' , 'ejs');
+
+app.get('/' , (req , res) => {
+    res.render('home');
+})
+
+app.get('/profile' , (req , res) => {
+    user = {
+         name:'Anil',
+         email:'anil@gmail.com',
+         city:'Noida',
+         skills:['HTML' , 'Css' , 'Js' , 'Node Js' , 'Java']
+    }
+    res.render('profile' , {user});
+});
+
+
+app.listen(port , (err) => {
+if(!err){
+    console.log(`Server is running on the port::${port}`);
+}
+});
+
+
+
