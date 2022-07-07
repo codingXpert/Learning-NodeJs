089a5c193d0a3de2cc5c2529b1db1c6a427b64f6
Vivek <vivekraj78965.vr@gmail.com>
2022-07-07 16:52:40 +0530

    Router level middleware

---
+++ middleware3.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+
+//creating a middleware
+const reqFilters = (req , res , next) => {
+    if(!req.query.age){
+        res.send('<h1>Please Provide Age</h1>');
+    }
+
+    else if(req.query.age < 18){
+res.send('<h1>You can not access this page</h1>');
+    }
+
+    else{
+        next();
+    }
+}
+
+app.get('/' , (req,res) => {
+    res.send('<h1>Welcome to home page</h1>');
+});
+
+
+//appliying middleware on single route
+app.get('/user' , reqFilters , (req , res) => {
+    res.send('<h1>Welcome to users page</h1>')
+});
+
+app.listen(8000 , (err) => {
+    if(!err){
+        console.log('Server is running on the port::8000');
+    }
+});
